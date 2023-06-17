b243b0c5e09105a7380ff3fb717ab84c8ccb3b56
cofkaa <cofkaa@o2.pl>
2023-06-17 07:51:08 +0200

    SalesValues - Interface + class

---
+++ GrossMarginApp/GrossMarginApp/DataModels/SalesValues.cs
@@ -0,0 +1,39 @@
+﻿using GrossMarginApp.Iterfaces;
+
+namespace GrossMarginApp.DataModels
+{
+    public class SalesValues : ISalesValues
+    {
+        public float SalesInvoicesValue { get; private set; }
+        public float TransportCosts { get; private set; }
+        public float ProductionCosts { get; private set; }
+        public float VariableCosts { get; private set; }
+        public float TotalCosts => TransportCosts + ProductionCosts + VariableCosts;
+        public float GrossMargin => SalesInvoicesValue - TotalCosts;
+        public float PercGrossMargin => SalesInvoicesValue != 0 ? GrossMargin / SalesInvoicesValue : 0;
+
+        public SalesValues()
+        {
+            this.SalesInvoicesValue = 0;
+            this.TransportCosts = 0;
+            this.ProductionCosts = 0;
+            this.VariableCosts = 0;
+        }
+        public void AddSalesInvoicesValue(float value)
+        {
+            SalesInvoicesValue += value;
+        }
+        public void AddTransportCosts(float value)
+        {
+            this.TransportCosts += value;
+        }
+        public void AddProductionCosts(float value)
+        {
+            this.ProductionCosts += value;
+        }
+        public void AddVariableCosts(float value)
+        {
+            this.VariableCosts += value;
+        }
+    }
+}
---
+++ GrossMarginApp/GrossMarginApp/Interfaces/ISalesValues.cs
@@ -0,0 +1,18 @@
+﻿namespace GrossMarginApp.Iterfaces
+{
+    public interface ISalesValues
+    {
+        float SalesInvoicesValue { get; }
+        float TransportCosts { get; }
+        float ProductionCosts { get; }
+        float VariableCosts { get; }
+        float TotalCosts { get; }
+        float GrossMargin { get; }
+        float PercGrossMargin { get; }
+
+        public void AddSalesInvoicesValue(float value) { }
+        public void AddTransportCosts(float value) { }
+        public void AddProductionCosts(float value) { }
+        public void AddVariableCosts(float value) { }
+    }
+}
